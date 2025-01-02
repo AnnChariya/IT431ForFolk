1e6aacdb9ea027d14dcea86e05bd9d751d13873a
witchaphatmumu <witchaphat.n@rmutsvmail.com>
2025-01-02 16:12:22 +0700

    แก้ไขไฟล์ delete.php #1

--- delete.php
+++ delete.php
@@ -29,7 +29,7 @@
     ?>
         <td width="190" bgcolor="#FFFF66">
                 <ul>
-                    <li><a href="insert_admin.php">เพิ่มข้อมูลรายการห้อง</a></li>
+                    <li><a href="insert_admin.php">เพิ่มข้อมูลรายการห้องทำงาน</a></li>
                     <li><a href="load_room_admin.php">แก้ไข-ลบข้อมูลรายการห้อง</a></li>
                     <li><a href="logout_admin.php">ออกจากระบบ</a></li>
                 </ul>
